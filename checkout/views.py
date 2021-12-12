b4f81bf99b505048d67391f7e2f8ceb2ca5bfbcc
DrogrinHunter <lukewillmore67@gmail.com>
2021-12-12 18:43:58 +0000

    added stripe

--- checkout/views.py
+++ checkout/views.py
@@ -12,7 +12,10 @@ def checkout(request):
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
-        'order_form': order_form
+        'order_form': order_form,
+        'stripe_public_key': 'pk_test_51K5wkSL30gvN7JUGoQBOIBYkJLgxXbUdnDUzDYFZMyTMchXdUMunP6Oi06k9IzyU8vTrvaadGAcUtZqSUJzOX3Jl00wh9WJTSt',
+        'client_secret': 'text client secret',
+
     }
 
     return render(request, template, context)
